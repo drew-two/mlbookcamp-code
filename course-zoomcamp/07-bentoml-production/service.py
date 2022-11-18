fb8166f109e3158613868d38e5e2cfc15edc4bed
Andrew Katoch <andrewvk7@gmail.com>
2022-11-17 22:42:23 -0500

    Finished week 7

--- course-zoomcamp/07-bentoml-production/service.py
+++ course-zoomcamp/07-bentoml-production/service.py
@@ -2,24 +2,28 @@ import numpy as np
 
 import bentoml
 from bentoml.io import JSON
+from bentoml.io import NumpyNdarray
 
+from typing import Set
 from pydantic import BaseModel
+from pydantic import constr
 
-model_ref = bentoml.xgboost.get("credit_risk_model:oyfd25cj3oijqdu5")
+model_ref = bentoml.xgboost.get("credit_risk_model:cacobpdflgrusaav")
 dv = model_ref.custom_objects['dictVectorizer']
 
 model_runner = model_ref.to_runner()
 
 svc = bentoml.Service("credit_risk_classifier", runners=[model_runner])
 
+JSONStr = constr(regex='^[A-z]+$')
 class CreditApplication(BaseModel):
     seniority: int
-    home: str
+    home: JSONStr
     time: int
     age: int
-    marital: str
-    records: str
-    job: str
+    marital: JSONStr
+    records: JSONStr
+    job: JSONStr
     expenses: int
     income: float
     assets: float
@@ -28,10 +32,12 @@ class CreditApplication(BaseModel):
     price: int
 
 @svc.api(input=JSON(pydantic_model=CreditApplication), output=JSON())
-async def classify(application_data):
+async def classify(credit_application):
+    application_data = credit_application.dict()
     vector = dv.transform(application_data)
     prediction = await model_runner.predict.async_run(vector)
     print(prediction)
+    # return { "status": "Approved"}
     result = prediction[0]
 
     if result > 0.5:
